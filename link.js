08579df841e571463b2a8ba4385a78c12dfeb6d1
ajoyab95 <130991585+ajoyab95@users.noreply.github.com>
2023-04-25 18:07:10 +0600

    Update link.js

--- link.js
+++ link.js
@@ -1,1 +1,4 @@
-const offerLink = "vnd.youtube://www.youtube.com/redirect?event=comments&redir_token=QUFFLUhqa3hRbWJKb0pUeUotWjh2LUxSWVFGVEREak9zUXxBQ3Jtc0ttb1BrYU9taXpQUGY0WGR1NVQwZkRZaFhORTFGX0x0ckFmdlFGU2ZMcExLYkhIQzB0SmJPVWVSYWM1ZzY3NlhoWTlzWFRucnhJTzZKM2xEb3BqSjQ3ZW1tNjlkVXFvbVpUUEZTdjR5eVdOSzM0WTJlWQ&q=https%3A%2F%2Fmwamgz.palatlaldate.com%2F%3Futm_source%3Dda57dc555e50572d%26s1%3D188152%26s2%3D1821451%26s3%3Dbx__%26s5%3Dtm%26j1%3D1&html_redirect=1 ";
+const offerLink = "vnd.youtube://www.youtube.com/redirect?event=comments&redir_token=QUFFLUhqa3BpSmxkNGtmOVlqODhHLWhSUkpNNF9fWExuUXxBQ3Jtc0ttUGpCM210UzQ4MDdOODhNRmRGc0tPRF9MZ3FLdXpyWjRNSmxYcFl5a2lhSmpQOFhfTlVjUXdmOWo5U0hUNHJpWl9xMVp3eDhsbnZUaUxFMm9LWl8zbks3eDhOanhvTFJReGo3LWdTMDJWT3Y4OWdmTQ&q=https%3A%2F%2Fv.ht%2FRE4UM&html_redirect=1";
+
+
+
